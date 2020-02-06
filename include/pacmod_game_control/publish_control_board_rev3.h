d64d433505ecb29e81b8f737a7db2ed431e2f339
as-snehagn <sganesh@autonomoustuff.com>
2020-02-06 15:05:24 -0600

    Imported upstream version '3.0.1' of 'upstream'

--- include/pacmod_game_control/publish_control_board_rev3.h
+++ include/pacmod_game_control/publish_control_board_rev3.h
@@ -4,12 +4,12 @@
 * This file is part of the PACMod ROS 1.0 driver which is released under the MIT license.
 * See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
 */
- 
-#ifndef PUBLISH_CONTROL_BOARD_REV3_H
-#define PUBLISH_CONTROL_BOARD_REV3_H
 
-#include "globals.h"
-#include "publish_control.h"
+#ifndef PACMOD_GAME_CONTROL_PUBLISH_CONTROL_BOARD_REV3_H
+#define PACMOD_GAME_CONTROL_PUBLISH_CONTROL_BOARD_REV3_H
+
+#include "pacmod_game_control/globals.h"
+#include "pacmod_game_control/publish_control.h"
 
 #include <pacmod_msgs/SteerSystemCmd.h>
 #include <pacmod_msgs/SystemCmdBool.h>
@@ -29,16 +29,19 @@ class PublishControlBoardRev3 :
     PublishControlBoardRev3();
     static void callback_shift_rpt(const pacmod_msgs::SystemRptInt::ConstPtr& msg);
     static void callback_turn_rpt(const pacmod_msgs::SystemRptInt::ConstPtr& msg);
+    static void callback_rear_pass_door_rpt(const pacmod_msgs::SystemRptInt::ConstPtr& msg);
 
     // Variables
     static int last_shift_cmd;
     static int last_turn_cmd;
+    static int last_rear_pass_door_cmd;
     static float last_brake_cmd;
 
   private:
     // private functions
     void publish_steering_message(const sensor_msgs::Joy::ConstPtr& msg);
     void publish_turn_signal_message(const sensor_msgs::Joy::ConstPtr& msg);
+    void publish_rear_pass_door_message(const sensor_msgs::Joy::ConstPtr& msg);
     void publish_shifting_message(const sensor_msgs::Joy::ConstPtr& msg);
     void publish_accelerator_message(const sensor_msgs::Joy::ConstPtr& msg);
     void publish_brake_message(const sensor_msgs::Joy::ConstPtr& msg);
@@ -47,9 +50,10 @@ class PublishControlBoardRev3 :
     // ROS Subscribers
     ros::Subscriber shift_sub;
     ros::Subscriber turn_sub;
+    ros::Subscriber rear_pass_door_sub;
 };
 
-}
-}
+}  // namespace Joystick
+}  // namespace AS
 
-#endif
+#endif  // PACMOD_GAME_CONTROL_PUBLISH_CONTROL_BOARD_REV3_H
